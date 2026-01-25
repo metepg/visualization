169d4e7e99f3030c7e806dc48f9dac7cdb3ca773
Mete Guneysel <52155024+metepg@users.noreply.github.com>
2026-01-25 16:36:43 +0200

    Remove throws Exception

--- server/src/main/java/com/metepg/visualization/security/SecurityConfig.java
+++ server/src/main/java/com/metepg/visualization/security/SecurityConfig.java
@@ -57,7 +57,7 @@ public class SecurityConfig {
     }
 
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)  {
         return authenticationConfiguration.getAuthenticationManager();
     }
 

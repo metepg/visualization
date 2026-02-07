d7825ea1d3234b9fdb0cce0d45cf530b00752baa
Mete Guneysel <52155024+metepg@users.noreply.github.com>
2026-02-07 09:48:50 +0200

    Make dev server work inside containers with MacOS & Podman

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 const proxyTarget = process.env.VITE_PROXY_TARGET || 'http://localhost:8080';
 
+// Watch and HMR settings needed for podman & MacOs when running everything inside container
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -13,6 +14,14 @@ export default defineConfig({
         changeOrigin: true,
         secure: false,
       }
+    },
+    watch: {
+      usePolling: true,
+      interval: 1000
+    },
+    strictPort: true,
+    hmr: {
+      clientPort: 5173
     }
   }
 });
